6ff6d7d314798eec3263eda82e3e8b74ca021a79
Hamisi-Esq <brivahamisi@gmail.com>
2022-10-07 15:23:32 +0300

    Models Updated

--- crsbackend/students_accounts/models.py
+++ crsbackend/students_accounts/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class ComplaintsForm(models.Model):
-    Complain_description = models.CharField(max_length=100)
-    Events_that_took_Place = models.TextField()
-    Consequence_suffered =models.TextField()
-    Spoken_to_someone = models.TextField()
-    Dissatisfied_with_Informal_complaint = models.TextField()
-    evidence = models.TextField() 
-    recommendation = models.TextField()
-    date = models.TextField()
-    status_of_complaint = models.TextField()
+    Complain_description = models.CharField(max_length=100, null=True)
+    Events_that_took_Place = models.TextField(max_length=200, null=True)
+    Consequence_suffered =models.TextField(max_length=200, null=True)
+    Spoken_to_someone = models.TextField(max_length=200, null=True)
+    Dissatisfied_with_Informal_complaint = models.TextField(max_length=200, null=True)
+    evidence = models.TextField(max_length=200, null=True) 
+    recommendation = models.TextField(max_length=200, null=True)
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    status_of_complaint = models.TextField(max_length=25, null=True)
 
     def __str__(self):
         return self.Complain_description
@@ -19,17 +19,17 @@ class ComplaintsForm(models.Model):
 class RegistrationForm(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
 
-    First_Name = models.CharField(max_length=100)
-    Last_Name = models.CharField(max_length=100)
+    First_Name = models.CharField(max_length=100, null=True)
+    Last_Name = models.CharField(max_length=100, null=True)
 
-    RegNo = models.CharField(max_length=100)
-    Course = models.CharField(max_length=100)
-    Campus = models.CharField(max_length=100)
-    Phone_Number = models.CharField(max_length=100)
-    Email = models.CharField(max_length=100)
-    Department = models.CharField(max_length=100)
-    Password = models.CharField(max_length=100)
-    Confirm_Password = models.CharField(max_length=100)
+    RegNo = models.CharField(max_length=100, null=True)
+    Course = models.CharField(max_length=100, null=True)
+    Campus = models.CharField(max_length=100, null=True)
+    Phone_Number = models.CharField(max_length=100, null=True)
+    Email = models.CharField(max_length=100, null=True)
+    Department = models.CharField(max_length=100, null=True)
+    Password = models.CharField(max_length=100, null=True)
+    Confirm_Password = models.CharField(max_length=100, null=True)
 
 
 # class RegistrationForm(models.Model):
