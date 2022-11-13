0612f0b53c8fcf0f26e409f99c7b2d4f8d7fc324
Hamisi-Esq <brivahamisi@gmail.com>
2022-11-13 23:54:22 +0300

    data

--- crsbackend/students_accounts/urls.py
+++ crsbackend/students_accounts/urls.py
@@ -17,8 +17,6 @@ router.register('complaints', ComplaintViewset, basename='complaints')
 router.register('feedbacks', FeedbackViewset, basename='feedbacks')
 router.register('appeals', AppealViewset, basename='appeals')
 router.register('general_issues', GeneralIssuesViewset, basename='general_issues')
-# router.register('user_details', user_details, basename='general_issues')
-# router.register('token-auth', views.obtain_auth_token)
 
 urlpatterns = [
     path('api/auth/login/', api.LoginAPI.as_view(), name='knox_login'),
